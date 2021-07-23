de393b8d7a35c438855f073d4cf8e14a7e3ba7f3
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 11:40:21 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+const { response } = require("express");
+
+dotenv.config();
+
+// Authentication
+const credentials = {
+    api_key: process.env.APIKEY,
+    username: process.env.USERNAME
+}
+
+const AfricaTalking = require("africastalking")(credentials);
+
+const sms = AfricaTalking.SMS;
+router.post("/", (req, res) => {
+    const { to, message} = req.body || res.status(400).json({
+        error: "Both 'to' and 'message' are required"
+    });
+    sms
+    .send({to, message, enque: true })
+    .then(response => {
+        
+    })
+})
