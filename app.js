997255738350051f05fce6ed4ed55371e57bb6f0
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 11:55:01 +0100

    updates

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const smsRoute = require("./index");
 const bodyParser = require("body-parser");
-const smsRouter = require("./index");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`listening to localhost: ${PORT}`));
+app.listen(PORT, () => console.log(`running on localhost:${PORT}`));
 
-app.use("/", smsRouter);
+app.use(bodyParser.json());
 
+app.use("/", smsRoute);
--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
-const { response } = require("express");
 
 dotenv.config();
 
-// Authentication
+// Get authentication
 const credentials = {
-    api_key: process.env.APIKEY,
-    username: process.env.USERNAME
-}
+  apiKey: process.env.APIKEY, // use your sandbox app API key for development in the test environment
+  username: process.env.USERNAME // use 'sandbox' for development in the test environment
+};
 
-const AfricaTalking = require("africastalking")(credentials);
+const AfricasTalking = require("africastalking")(credentials);
 
-const sms = AfricaTalking.SMS;
+const sms = AfricasTalking.SMS;
+// Send SMS route
 router.post("/", (req, res) => {
-    const { to, message} = req.body || res.status(400).json({
-        error: "Both 'to' and 'message' are required"
-    });
-    sms
-    .send({to, message, enque: true })
+  const { to, message } = req.body || res.status(400).json({error: "Both 'to' and 'message' are required"});
+  sms
+    .send({ to, message, enque: true })
     .then(response => {
-        console.log(response);
-        res.json(response);
+      console.log(response);
+      res.json(response);
     })
     .catch(error => {
-        console.log(error);
-        res.json(error.toString());
-    })
-})
+      console.log(error);
+      res.json(error.toString());
+    });
+});
+
 module.exports = router;
