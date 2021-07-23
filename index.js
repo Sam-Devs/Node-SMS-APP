4558b253e168ac537f97af39cadf72e9a4674825
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 13:13:50 +0100

    fixes

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 // Get authentication
 const credentials = {
   apiKey: process.env.APIKEY, // use your sandbox app API key for development in the test environment
-  username: process.env.USERNAME // use 'sandbox' for development in the test environment
+  username: process.env.USER_NAME // use 'sandbox' for development in the test environment
 };
-
+console.log(credentials);
 const AfricasTalking = require("africastalking")(credentials);
 
 const sms = AfricasTalking.SMS;
@@ -17,7 +17,7 @@ router.post("/", (req, res) => {
   if(!to || !message) res.status(400).json({error: "Both 'to' and 'message' are required"});
 
   sms
-    .send({ to, message, enque: true })
+    .send({ to, message, from: "SMS012", enqueue: true })
     .then(response => {
       console.log(response);
       res.json(response);
