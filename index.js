bf31e89f23b822ac90a753811f1e066a7c39d58b
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 11:42:18 +0100

    index.js

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ router.post("/", (req, res) => {
     sms
     .send({to, message, enque: true })
     .then(response => {
-        
+        console.log(response);
+        res.json(response);
+    })
+    .catch(error => {
+        console.log(error);
+        res.json(error.toString());
     })
 })
+module.exports = router;
