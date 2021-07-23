b95315060464866b85c1fdf23dbf96b072e38afc
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 11:49:43 +0100

    app.js

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const smsRouter = require("./index");
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`listening to localhost: ${PORT}`));
+
+app.use("/", smsRouter);
+
